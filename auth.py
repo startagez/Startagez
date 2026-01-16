af688f07fdca08fdeb4e06909e7e3412c3f9ca1d
startagez <damaniobie@gmail.com>
2026-01-15 19:36:49 -0500

    authentication

---
+++ auth.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, render_template, request, redirect, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, logout_user
+from models import User
+from app import db
+
+auth_bp = Blueprint("auth", __name__)
+
+@auth_bp.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        user = User(
+            username=request.form["username"],
+            password=generate_password_hash(request.form["password"])
+        )
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("auth.login"))
+    return render_template("register.html")
+
+@auth_bp.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        user = User.query.filter_by(username=request.form["username"]).first()
+        if user and check_password_hash(user.password, request.form["password"]):
+            login_user(user)
+            return redirect(url_for("index"))
+    return render_template("login.html")
+
+@auth_bp.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
